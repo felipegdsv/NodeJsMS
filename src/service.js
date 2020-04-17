9eec6c99ac8b6220b9f93c53dbf1c89acbae7aba
felipe vieira <felipegdsv@gmail.com>
2020-04-17 06:02:54 -0300

    Commit inicial

---
+++ src/service.js
@@ -0,0 +1,10 @@
+//Creating a Server to Accept Requests
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 3000;
+
+const routes = require('./api/routes');
+routes(app);
+app.listen(port, function() {
+   console.log('Server started on port: ' + port);
+});
